8b77e9eb5ed41c1be77074ec74a8b824592d747e
omoyeni-babatunde <babatundeabike3@gmail.com>
2026-04-03 01:16:49 +0100

    Create StockPriceTrackerApp.swift

---
+++ StockPriceTracker/StockPriceTracker/StockPriceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockPriceTrackerApp.swift
+//  StockPriceTracker
+//
+//  Created by Sharon Omoyeni Babatunde on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct StockPriceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StockPriceTrackerView()
+        }
+    }
+}
