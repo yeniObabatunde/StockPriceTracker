a816b09171c28dc7b8736f585336c7be22e26359
omoyeni-babatunde <babatundeabike3@gmail.com>
2026-04-03 01:03:37 +0100

    constant file created for constant strings

---
+++ StockPriceTracker/StockPriceTracker/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  StockPriceTracker
+//
+//  Created by Sharon Omoyeni Babatunde on 02/04/2026.
+//
+
+import Foundation
+
+public enum Constants {
+  public enum App {
+    public static let socketURL: String = "wss://ws.postman-echo.com/raw"
+  }
+}
